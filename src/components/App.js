d80cc412cd16df0347227e91a8433079cf7e2a0d
VikasVerma-15 <97469119+VikasVerma-15@users.noreply.github.com>
2025-12-29 17:55:14 +0530

    Refactor Tooltip usage in App component

--- src/components/App.js
+++ src/components/App.js
@@ -4,16 +4,22 @@ import Tooltip from "./Tooltip";
 function App() {
   return (
     <div style={{ padding: "50px" }}>
-      <h2 className="tooltip">
-        <Tooltip text="Tooltip for heading">Hover over me</Tooltip>
+      <h2>
+        <Tooltip text="This is a tooltip" alwaysRender={false}>
+          Hover over me
+        </Tooltip>
       </h2>
 
-      <p className="tooltip">
-        <Tooltip text="Tooltip for paragraph">Hover over me to see another tooltip</Tooltip>
+      <p>
+        <Tooltip text="Paragraph tooltip" alwaysRender={false}>
+          Hover over me to see another tooltip
+        </Tooltip>
       </p>
 
-      <button className="tooltip">
-        <Tooltip text="Tooltip for button">Hover over this button</Tooltip>
+      <button>
+        <Tooltip text="Button tooltip" alwaysRender={false}>
+          Hover over this button
+        </Tooltip>
       </button>
     </div>
   );
