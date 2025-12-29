d6b54807da7414e52318d513008fd9b02b71002c
VikasVerma-15 <97469119+VikasVerma-15@users.noreply.github.com>
2025-12-29 17:52:24 +0530

    Refactor Tooltip components for better styling

--- src/components/App.js
+++ src/components/App.js
@@ -4,22 +4,16 @@ import Tooltip from "./Tooltip";
 function App() {
   return (
     <div style={{ padding: "50px" }}>
-      <h2>
-        <Tooltip text="Tooltip for heading">
-          Hover over me
-        </Tooltip>
+      <h2 className="tooltip">
+        <Tooltip text="Tooltip for heading">Hover over me</Tooltip>
       </h2>
 
-      <p>
-        <Tooltip text="Tooltip for paragraph">
-          Hover over me to see another tooltip
-        </Tooltip>
+      <p className="tooltip">
+        <Tooltip text="Tooltip for paragraph">Hover over me to see another tooltip</Tooltip>
       </p>
 
-      <button>
-        <Tooltip text="Tooltip for button">
-          Hover over this button
-        </Tooltip>
+      <button className="tooltip">
+        <Tooltip text="Tooltip for button">Hover over this button</Tooltip>
       </button>
     </div>
   );
