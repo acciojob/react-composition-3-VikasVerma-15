1d0a2f3c0fc1be6aa9c411ad3f9fc38073244cd8
Vikas Verma <vikasverma15@gmail.com>
2026-01-09 16:05:25 +0530

    update

--- src/components/Tooltip.js
+++ src/components/Tooltip.js
@@ -1,39 +1,18 @@
 import React, { useState } from "react";
 
-function Tooltip({ text, children, alwaysRender = false }) {
+function Tooltip({ text, children }) {
   const [visible, setVisible] = useState(false);
-
-  const containerStyle = {
-    display: "inline-block",
-    position: "relative",
-    cursor: "pointer",
-  };
-
-  const tooltipStyle = {
-    position: "absolute",
-    top: "100%",
-    left: "50%",
-    transform: "translateX(-50%)",
-    backgroundColor: "black",
-    color: "white",
-    padding: "5px 8px",
-    borderRadius: "4px",
-    marginTop: "5px",
-    whiteSpace: "nowrap",
-    fontSize: "14px",
-    zIndex: 10,
-    transition: "opacity 0.2s ease-in-out",
-  };
 
   return (
     <div
-      style={containerStyle}
       onMouseEnter={() => setVisible(true)}
       onMouseLeave={() => setVisible(false)}
     >
-      {children}
-      {(alwaysRender || visible) && (
-        <div className="tooltiptext" style={tooltipStyle}>
+      {/* isolate children text */}
+      <span>{children}</span>
+
+      {visible && (
+        <div className="tooltiptext">
           {text}
         </div>
       )}
@@ -42,6 +21,3 @@ function Tooltip({ text, children, alwaysRender = false }) {
 }
 
 export default Tooltip;
-
-
-
