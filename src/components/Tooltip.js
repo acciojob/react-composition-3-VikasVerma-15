e17567890c2870682d5767a3238f1773417f43b3
Vikas Verma <vikasverma15@gmail.com>
2026-01-09 15:48:26 +0530

    update

--- src/components/Tooltip.js
+++ src/components/Tooltip.js
@@ -1,21 +1,47 @@
 import React, { useState } from "react";
 
-function Tooltip({ text, children }) {
+function Tooltip({ text, children, alwaysRender = false }) {
   const [visible, setVisible] = useState(false);
 
-  return React.cloneElement(children, {
-    className: "tooltip",
-    onMouseEnter: () => setVisible(true),
-    onMouseLeave: () => setVisible(false),
-    children: (
-      <>
-        {children.props.children}
-        {visible && <div className="tooltiptext">{text}</div>}
-      </>
-    )
-  });
+  const containerStyle = {
+    display: "inline-block",
+    position: "relative",
+    cursor: "pointer",
+  };
+
+  const tooltipStyle = {
+    position: "absolute",
+    top: "100%",
+    left: "50%",
+    transform: "translateX(-50%)",
+    backgroundColor: "black",
+    color: "white",
+    padding: "5px 8px",
+    borderRadius: "4px",
+    marginTop: "5px",
+    whiteSpace: "nowrap",
+    fontSize: "14px",
+    zIndex: 10,
+    transition: "opacity 0.2s ease-in-out",
+  };
+
+  return (
+    <div
+      style={containerStyle}
+      onMouseEnter={() => setVisible(true)}
+      onMouseLeave={() => setVisible(false)}
+    >
+      {children}
+      {(alwaysRender || visible) && (
+        <div className="tooltiptext" style={tooltipStyle}>
+          {text}
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default Tooltip;
+
 
 
